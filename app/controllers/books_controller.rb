62dea4fec3794ac2d82f89f2b362b7b8aad22403
vagrant <vagrant@localhost.localdomain>
2020-05-26 11:59:00 +0000

    [finish]5/26

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def index
   	@books = Book.all
+  	@book = Book.new
   end
 
   def show
@@ -26,6 +27,12 @@ class BooksController < ApplicationController
   	book = Book.find(params[:id])
   	book.update(book_params)
   	redirect_to book_path(book)
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
   end
 
   private
