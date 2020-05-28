c4062e29c732fb137f65f543e7705ef90bd3181d
vagrant <vagrant@localhost.localdomain>
2020-05-28 04:18:20 +0000

    [finihs]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,8 +15,13 @@ class BooksController < ApplicationController
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	if book.save
+      flash[:notice] = "Book was successfully created."
+  	  redirect_to book_path(book.id)
+    else
+      flash.now[:alert] = "あ"
+      render :index
+    end
   end
 
   def edit
@@ -25,7 +30,8 @@ class BooksController < ApplicationController
 
   def update
   	book = Book.find(params[:id])
-  	book.update(book_params)
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
   	redirect_to book_path(book)
   end
 
