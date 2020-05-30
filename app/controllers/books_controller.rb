85c183cdad09252133055c2bab2fbb408c1e08f8
愿山聖 <yoyo@sunaoyamaseinoMacBook-Pro.local>
2020-05-30 19:24:08 +0900

    bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,13 +14,13 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	if book.save
+  	@book = Book.new(book_params)
+  	if @book.save
       flash[:notice] = "Book was successfully created."
-  	  redirect_to book_path(book.id)
+  	  redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = "あ"
-      render :index
+      @books = Book.all
+      render action: :index
     end
   end
 
